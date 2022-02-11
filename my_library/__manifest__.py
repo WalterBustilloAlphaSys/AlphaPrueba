80ec0f8aaa9d2e524f1ab81085f144cb0e707a8d
WalterBustilloAlphaSys <97619091+WalterBustilloAlphaSys@users.noreply.github.com>
2022-02-11 09:17:38 -0400

    [ADD] 8-5

--- my_library/__manifest__.py
+++ my_library/__manifest__.py
@@ -19,6 +19,7 @@ Description related to library.
         'views/library_book_categ.xml',
         'views/library_book_rent.xml',
         'wizard/library_book_rent_wizard.xml',
+        'wizard/library_book_return_wizard.xml',
     ],
     # This demo data files will be loaded if db initialize with demo data (commented because file is not used in this example)
     # 'demo': [
