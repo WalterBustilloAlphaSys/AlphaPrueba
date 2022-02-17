f3d5fc1fe327b0781aed0e3fc3af7d00d0a73ccd
WalterBustilloAlphaSys <97619091+WalterBustilloAlphaSys@users.noreply.github.com>
2022-02-17 13:15:00 -0400

    [ADD] 13-3

--- my_library/controllers/main.py
+++ my_library/controllers/main.py
@@ -4,16 +4,9 @@ from odoo.http import request
 
 
 class Main(http.Controller):
-    @http.route('/my_library/books', type='http', auth='none')
-    def books(self):
-        books = request.env['library.book'].sudo().search([])
-        html_result = '<html><body><ul>'
-        for book in books:
-            html_result += "<li> %s </li>" % book.name
-        html_result += '</ul></body></html>'
-        return html_result
-
-    @http.route('/my_library/books/json', type='json', auth='none')
-    def books_json(self):
-        records = request.env['library.book'].sudo().search([])
-        return records.read(['name'])
+    @http.route('/books', type='http', auth="user", website=True)
+    def library_books(self):
+        return request.render(
+            'my_library.books', {
+                'books': request.env['library.book'].search([]),
+            })
