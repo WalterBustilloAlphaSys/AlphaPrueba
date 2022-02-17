382ac79d3ccc074b3d04842b417d18889dd3df44
WalterBustilloAlphaSys <97619091+WalterBustilloAlphaSys@users.noreply.github.com>
2022-02-17 14:30:19 -0400

    [ADD] 14-5

--- my_library/__manifest__.py
+++ my_library/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-        'name': "My Library",  # Module title
+    'name': "My Library",  # Module title
     'summary': "Manage books easily",  # Module subtitle phrase
     'description': """
 Manage Library
@@ -11,16 +11,21 @@ Description related to library.
     'website': "http://www.example.com",
     'category': 'Tools',
     'version': '14.0.1',
-    'depends': ['base', 'web'],
+    'depends': ['base', 'website'],
 
     'data': [
         'security/groups.xml',
         'security/ir.model.access.csv',
         'views/library_book.xml',
-        'views/templates.xml'
+        'views/templates.xml',
+        'views/snippets.xml'
     ],
-    # This demo data files will be loaded if db initialize with demo data (commented becaues file is not added in this example)
+
+    # This demo data files will be loaded if db initialize with demo data (commented because file is not added in this example)
     # 'demo': [
     #     'demo.xml'
     # ],
 }
+
+
+
