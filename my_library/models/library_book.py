01389d129c2bfb9ae89d2def833186ded4836ec0
WalterBustilloAlphaSys <97619091+WalterBustilloAlphaSys@users.noreply.github.com>
2022-02-18 11:40:34 -0400

    [ADD] 15-0

--- my_library/models/library_book.py
+++ my_library/models/library_book.py
@@ -6,18 +6,17 @@ class LibraryBook(models.Model):
     _name = 'library.book'
     _description = 'Library Book'
 
+    _order = 'date_release desc, name'
+
     name = fields.Char('Title', required=True)
+    short_name = fields.Char('Short Title')
     date_release = fields.Date('Release Date')
     author_ids = fields.Many2many('res.partner', string='Authors')
-    image = fields.Binary(attachment=True)
-    html_description = fields.Html()
-    book_issue_ids = fields.One2many('book.issue', 'book_id')
 
-
-class LibraryBookIssues(models.Model):
-    _name = 'book.issue'
-
-    book_id = fields.Many2one('library.book', required=True)
-    submitted_by = fields.Many2one('res.users')
-    issue_description = fields.Text()
-
+    def name_get(self):
+        """ This method used to customize display name of the record """
+        result = []
+        for record in self:
+            rec_name = "%s (%s)" % (record.name, record.date_release)
+            result.append((record.id, rec_name))
+        return result
