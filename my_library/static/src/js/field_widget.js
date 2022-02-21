ba589529fe3a9cde9edf29d66e65fae3ce861e75
WalterBustilloAlphaSys <97619091+WalterBustilloAlphaSys@users.noreply.github.com>
2022-02-21 08:51:01 -0400

    [ADD] 15-2

--- my_library/static/src/js/field_widget.js
+++ my_library/static/src/js/field_widget.js
@@ -4,6 +4,12 @@ odoo.define('my_field_widget', function (require) {
 var AbstractField = require('web.AbstractField');
 var fieldRegistry = require('web.field_registry');
 
+var core = require('web.core');
+
+var qweb = core.qweb;
+
+
+
 var colorField = AbstractField.extend({
     className: 'o_int_colorpicker',
     tagName: 'span',
@@ -17,16 +23,8 @@ var colorField = AbstractField.extend({
     },
     _renderEdit: function () {
         this.$el.empty();
-        for (var i = 0; i < this.totalColors; i++ ) {
-            var className = "o_color_pill o_color_" + i;
-            if (this.value === i ) {
-                className += ' active';
-            }
-            this.$el.append($('<span>', {
-                'class': className,
-                'data-val': i,
-            }));
-        }
+        var pills = qweb.render('FieldColorPills', {widget: this});
+        this.$el.append(pills);
     },
     _renderReadonly: function () {
         var className = "o_color_pill active readonly o_color_" + this.value;
