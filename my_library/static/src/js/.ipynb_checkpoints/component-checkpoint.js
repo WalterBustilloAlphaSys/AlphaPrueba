3ea5bdc68772a44ce913a3be91adf42d9742de90
walter <walter.bustillo@alphasys.com.bo>
2022-02-22 20:04:15 +0000

    [ADD] 16-3

--- my_library/static/src/js/.ipynb_checkpoints/component-checkpoint.js
+++ my_library/static/src/js/.ipynb_checkpoints/component-checkpoint.js
@@ -5,37 +5,37 @@ odoo.define('my.component', function (require) {
     const { xml } = owl.tags;
 
     class MyComponent extends Component {
+
         static template = xml`
             <div class="bg-info text-center p-2">
-
-                <i class="fa fa-close p-1 float-right"
-                    style="cursor:pinter;"
-                    t-on-click="onPrevious"> </i>              
-                <b t-esc="messageList[Math.abs(
-                    state.currentIndex%4)]">
+                <i class="fa fa-arrow-left p-1"
+                    style="cursor: pointer;"
+                    t-on-click="onPrevious"> </i>
+                <b t-esc="messageList[Math.abs(state.currentIndex%4)]"/>
                 <i class="fa fa-arrow-right p-1"
                     style="cursor: pointer;"
                     t-on-click="onNext"> </i>
-
                 <i class="fa fa-close p-1 float-right"
-                    style="cursor:pinter;"
+                    style="cursor: pointer;"
                     t-on-click="onRemove"> </i>
-            
-                </div>`
-        cosntructor() {
+            </div>`
+
+        constructor() {
+            console.log('CALLED:> constructor');
             super(...arguments);
-            this.messageList= [
+            this.messageList = [
                 'Hello World',
                 'Welcome to Odoo',
                 'Odoo is awesome',
                 'You are awesome too'
             ];
-            this.state = userState({ currentIndex: 0});
+            this.state = useState({ currentIndex: 0 });
         }
-        onRemove(ev){
+
+        onRemove(ev) {
             this.destroy();
         }
-        onNext(ev){
+        onNext(ev) {
             this.state.currentIndex++;
         }
         onPrevious(ev) {
