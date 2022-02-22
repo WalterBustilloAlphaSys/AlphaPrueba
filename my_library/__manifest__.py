131fabec1c6f2307067e2f439d0f6f21f7fc5e20
WalterBustilloAlphaSys <97619091+WalterBustilloAlphaSys@users.noreply.github.com>
2022-02-22 11:04:11 -0400

    [ADD] 16-2

--- my_library/__manifest__.py
+++ my_library/__manifest__.py
@@ -11,12 +11,13 @@ Description related to library.
     'website': "http://www.example.com",
     'category': 'Tools',
     'version': '14.0.1',
-    'depends': ['base'],
+    'depends': ['base', 'web'],
 
     'data': [
         'security/groups.xml',
         'security/ir.model.access.csv',
-        'views/library_book.xml'
+        'views/library_book.xml',
+        'views/templates.xml'
     ],
     # This demo data files will be loaded if db initialize with demo data (commented becaues file is not added in this example)
     # 'demo': [
