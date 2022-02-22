5af9278c3b543922d6e35f05ef3a49ca62f41c1c
walter <walter.bustillo@alphasys.com.bo>
2022-02-22 17:39:31 +0000

    [MOD] 16-1

--- my_library/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ my_library/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "My Library",  # Module title
+        'name': "My Library",  # Module title
     'summary': "Manage books easily",  # Module subtitle phrase
     'description': """
 Manage Library
@@ -16,8 +16,7 @@ Description related to library.
     'data': [
         'security/groups.xml',
         'security/ir.model.access.csv',
-        'views/library_book.xml',
-        'views/templates.xml'
+        'views/library_book.xml'
     ],
     # This demo data files will be loaded if db initialize with demo data (commented becaues file is not added in this example)
     # 'demo': [
